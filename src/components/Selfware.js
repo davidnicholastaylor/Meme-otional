3b62efe532e50f3ee63e8a8839e38f494fce2d95
davidnicholastaylor <dntaylor14@gmail.com>
2018-09-14 15:54:18 -0500

    created logout function

--- src/components/Selfware.js
+++ src/components/Selfware.js
@@ -1,13 +1,17 @@
+import { Route, Redirect } from 'react-router-dom'
 import React, { Component } from "react"
 import Navbar from "./nav/Navbar"
 import AppViews from "./AppViews"
+import login from "./Login"
 
 
 class Selfware extends Component {
+    
     render() {
         return (
             <React.Fragment>
                 <div>
+                    
                     <Navbar />
                     <AppViews />
                 </div>
--- src/components/nav/Navbar.js
+++ src/components/nav/Navbar.js
@@ -1,13 +1,25 @@
+import { Route, Redirect } from 'react-router-dom'
 import React, { Component } from "react"
 import { Link } from "react-router-dom"
 import "./Navbar.css"
 
 
 class NavBar extends Component {
+    // logout = () => {
+    //     sessionStorage.removeItem("credentials")
+    //     return <Redirect to ="/login" />
+    //   }
+    state = {
+        logout: false
+    }
+
+    logoutUser = () => {
+        this.setState({logout: true})
+    }
     render() {
         return (
-            
             <nav >
+                { this.state.logout === false &&
                 <ul >
                     <li >
                         <Link  to="/days">Day</Link>
@@ -18,7 +30,16 @@ class NavBar extends Component {
                     <li >
                         <Link  to="/years">Year</Link>
                     </li>
+                    <button onClick={this.logoutUser}>Logout</button>
                 </ul>
+
+            }
+            { this.state.logout === true &&
+            <div>
+                {this.setState({logout: false})}
+                <Redirect to="/login" />
+                </div>
+            }
             </nav>
         )
     }
