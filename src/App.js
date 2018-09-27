8dc09808f91ca2608c5eac6980270bfcd2378cf1
davidnicholastaylor <dntaylor14@gmail.com>
2018-09-27 11:45:59 -0500

    added logo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 
 import React, { Component } from 'react';
 import './App.css';
+import emoji from './components/images/emoji-tongue.png'
 
 class App extends Component {
   
@@ -8,6 +9,7 @@ class App extends Component {
     return (
       <div className="App">
         <header className="App-header">
+         <img src={emoji} className="App-logo" alt="logo" />
           <h1 className="App-title">Meme-otional</h1>
         </header>
       </div>
